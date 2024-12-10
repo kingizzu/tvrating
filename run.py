1ebb9bf754d68dff174f26b64c20b737094d2756
kingizzu <138001977+kingizzu@users.noreply.github.com>
2024-12-10 16:46:20 +0800

    Update run.py

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ program_ratings_dict = read_csv_to_dict(file_path)
 
 # Print the result (you can also return or process it further)
 for program, ratings in program_ratings_dict.items():
-    st.print(f"'{program}': {ratings},")
+    print(f"'{program}': {ratings},")
 
 import random
 
