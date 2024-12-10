dd170241a829ea764072f7dcb40ed99013972ae8
kingizzu <138001977+kingizzu@users.noreply.github.com>
2024-12-10 17:45:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -128,8 +128,8 @@ def genetic_algorithm(initial_schedule, generations=GEN, population_size=POP, cr
 ##################################################### MAIN LOGIC ###################################################################################
 
 # User inputs for crossover and mutation rates
-CO_R = st.slider("Crossover Rate", 0.0, 0.95, 0.8)  # Crossover Rate with default of 0.8
-MUT_R = st.slider("Mutation Rate", 0.01, 0.05, 0.02)  # Mutation Rate with default of 0.02
+CO_R = st.number_input("Crossover Rate", min_value=0.0, max_value=0.95, value=0.8, step=0.01)  # Allows numeric input
+MUT_R = st.number_input("Mutation Rate", min_value=0.01, max_value=0.05, value=0.02, step=0.01)  # Allows numeric input
 
 # Execute button
 if st.button("Run Genetic Algorithm"):
